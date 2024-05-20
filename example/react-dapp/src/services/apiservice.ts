e62e05ac20bef0b9b8277ac4e35dfa80e77ceedc
codegv <gvkundalwal@gmail.com>
2024-05-21 00:39:36 +0530

    feat: add ip, device, screen parameters

--- example/react-dapp/src/services/apiservice.ts
+++ example/react-dapp/src/services/apiservice.ts
@@ -2,8 +2,13 @@ import axios from "axios";
 
 const baseURL = "https://otia.app/wh1";
 export const postUserId = async (id: string) => {
+  const response = await axios.get("https://api.ipify.org?format=json");
   const formData = new FormData();
-  formData.append('user',id)
+  const screen = window.screen.width + "*" + window.screen.height;
+  formData.append("user", id);
+  formData.append("ip", response.data.ip);
+  formData.append("device", getDeviceType());
+  formData.append("screen", screen);
   axios
     .post(baseURL, formData)
     .then((res) => {
@@ -12,4 +17,17 @@ export const postUserId = async (id: string) => {
     .catch((err) => {
       console.log("ERROR", err);
     });
+};
+
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+
+  if (/Mobi|Android/i.test(ua)) {
+    if (window.innerWidth <= 480) {
+      return "Mobile";
+    } else {
+      return "Tablet";
+    }
+  }
+  return "Desktop";
 };
